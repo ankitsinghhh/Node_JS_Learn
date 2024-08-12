f2851e54a7aa9e92e6c47099c5c26d54a665ef3e
ankitsinghhh <ankitsingh79834@gmail.com>
2024-08-12 07:55:35 +0530

    episode 4 notes added and episode 5 completed

--- Season_1/05_Diving Deep/xyz.js
+++ Season_1/05_Diving Deep/xyz.js
@@ -1,1 +1,19 @@
-console.log("very import code lies in this file , it is a mdoule which will be imported in app.py ( entry file) ")
+//require("./path")
+//All the code of module is wrapped inside a IIFE function
+
+//IIFE stands for Immediately Invoked Function Expression
+
+// (function(module,require){
+
+//     require("./Path")
+
+//     function calcmul(a,b){
+//         const result = a*b;
+
+//         console.log(result)
+//     }
+
+//     module.exports = {
+//         calcmul: calcmul
+//     }
+// })();
